552a8218395c744446b12d2e7a2662ce19ddecbb
pekspro <msn@pekspro.com>
2024-06-22 11:56:09 +0200

    Changes version to 0.0.2-preview01.

--- Source/Library/Pekspro.BuildInformationGenerator/Constants.cs
+++ Source/Library/Pekspro.BuildInformationGenerator/Constants.cs
@@ -3,6 +3,6 @@ namespace Pekspro.BuildInformationGenerator;
 public static class Constants
 {
     // Note. Should also be changed in Version.props.
-    //public const string Version = "0.0.1-preview10";
-    public const string Version = "0.1.0";
+    public const string Version = "0.0.2-preview01";
+    //public const string Version = "0.1.0";
 }

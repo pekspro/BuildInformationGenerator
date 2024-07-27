0be5ba9a9db95b8fbc4e883ceb3eefb25b712ace
pekspro <msn@pekspro.com>
2024-07-27 19:52:15 +0200

    Changes version to 0.3.0.

--- Source/Library/Pekspro.BuildInformationGenerator/Constants.cs
+++ Source/Library/Pekspro.BuildInformationGenerator/Constants.cs
@@ -4,5 +4,5 @@ public static class Constants
 {
     // Note. Should also be changed in Version.props.
     //public const string Version = "0.0.2-preview02";
-    public const string Version = "0.2.0";
+    public const string Version = "0.3.0";
 }
